72cfa6bc41a944d83db19688f6d853b9d0fd4734
JER079 <35400850+JER079@users.noreply.github.com>
2018-03-04 20:24:57 +0100

    Add files via upload

---
+++ BiometricAuthentication/Common/Session.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BiometricAuthentication.Business
+{
+    public class Session
+    {
+        private const double ExpiryTimeInSeconds = 600; 
+
+        public Session()
+        {
+            SessionId = Guid.NewGuid();
+            SessionStartTime = DateTime.Now;
+            SessionEndTime = DateTime.Now.AddSeconds(ExpiryTimeInSeconds);
+            Console.WriteLine("New Session ID, Session Start Time and Expiry Time configured");
+        }
+        //
+        public Guid SessionId;
+        public DateTime SessionStartTime;
+        public DateTime SessionEndTime;
+        public bool IsExpired => DateTime.Now > SessionEndTime;
+    }
+}
